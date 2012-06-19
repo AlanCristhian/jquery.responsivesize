41f117ebd234b0309bc8e98715e8d8c36e0a18ad
Alan Cristhian <alan.cristh@gmail.com>
2012-06-19 17:07:48 -0300

    add avatar7.jpg file and add it in test.html

--- jquery.responsivesize.coffee
+++ jquery.responsivesize.coffee
@@ -1,31 +1,60 @@
-### Change the font size so that these are not small in screen resolutions greater than 96ppi such as the display of the new iPad.
+### Change the font size so that these are not small in screen resolutions greater than 96DPI such as the display of the new iPad.
 
-By default, if the resolution is less than 96ppi then does nothing. But if greater than this, then enlarges the font to a size equivalent to a 96ppi screen.
+By default, if the resolution is less than 96DPI then does nothing. But if greater than this, then enlarges the font to a size equivalent to a 96DPI screen.
 
-You can customize the default PPI. ###
-
+You can customize the default DPI. ###
 
 (($)->
 	$.fn.responsivesize = (options) ->
 		defaults =
 			density: 96
-		$.extend defaults, options
+			adaptFont: true
+			adaptImages: true
+
+		$.extend(defaults, options)
+
+		### devuelve los dpi de la pantalla del usuario ###
+		getDPI = ->
+			element = $("<div style='float: left; margin: 0; padding: 0; visibility: hidden; width: 1in;'></div>")
+			element.appendTo("body")
+			DPI = element.outerWidth()
+			element.remove()
+			DPI
+
+		### devuelve el tamaño de fuente en pixeles ###
+		getFontSize = (element) ->
+			size = element.css("font-size")
+			size = Number(size.substr(0, size.length - 2))
+			size
+
+		### ajusta el tamaño de fuente ###
+		adaptFontSize = (element)->
+			DPI = getDPI()
+			if DPI > defaults.density
+				nFontSize = Math.round(DPI * getFontSize(element) / defaults.density) + "px"
+			element.css("font-size", nFontSize)
+			return
+
+		### ajusta el tamaño de las imágenes ###
+		adaptImagesSize = (element)->
+			DPI = getDPI()
+			if DPI > defaults.density
+				images = $("img", element)
+				images.each ->
+					$this = $(@)
+					newHeight = Math.round(DPI * $this.outerHeight() / defaults.density) + "px"
+					newWidth = Math.round(DPI * $this.outerWidth() / defaults.density) + "px"
+					$this.css("height": newHeight, "width": newWidth)
+			return
+
+		### aplica los cambios a los elementos seleccionados ###
 		@.each ->
-			$this = $ @
-			getPPI = ->
-				element = $ "<div style='float: left; margin: 0; padding: 0; visibility: hidden; width: 1in;'></div>"
-				element.appendTo "body"
-				ppi = element.outerWidth()
-				element.remove()
-				ppi
-			getFontSize = ->
-				size = $this.css "font-size"
-				size = Number size.substr(0, size.length - 2)
-				size
-			ppi = getPPI()
-			if ppi > defaults.density
-				nFontSize = Math.round(ppi * getFontSize() / defaults.density) + "px"
-			$this.css "font-size", nFontSize
-			true
+			$this = $(@)
+
+			if defaults.adaptFont
+				adaptFontSize($this)
+			if defaults.adaptImages
+				adaptImagesSize($this)
+			return
 	return
 )(jQuery)
--- jquery.responsivesize.js
+++ jquery.responsivesize.js
@@ -1,44 +1,80 @@
 
-/* Change the font size so that these are not small in screen resolutions greater than 96ppi such as the display of the new iPad.
 
-By default, if the resolution is less than 96ppi then does nothing. But if greater than this, then enlarges the font to a size equivalent to a 96ppi screen.
+/* Change the font size so that these are not small in screen resolutions greater than 96DPI such as the display of the new iPad.
 
-You can customize the default PPI.
-*/
+By default, if the resolution is less than 96DPI then does nothing. But if greater than this, then enlarges the font to a size equivalent to a 96DPI screen.
 
+You can customize the default DPI. */
 
 (function() {
 
 	(function($) {
 		$.fn.responsivesize = function(options) {
-			var defaults;
+			var adaptFontSize, adaptImagesSize, defaults, getDPI, getFontSize;
 			defaults = {
-				density: 96
+				density: 96,
+				adaptFont: true,
+				adaptImages: true
 			};
 			$.extend(defaults, options);
-			return this.each(function() {
-				var $this, getFontSize, getPPI, nFontSize, ppi;
-				$this = $(this);
-				getPPI = function() {
-					var element, ppi;
-					element = $("<div style='float: left; margin: 0; padding: 0; visibility: hidden; width: 1in;'></div>");
-					element.appendTo("body");
-					ppi = element.outerWidth();
-					element.remove();
-					return ppi;
-				};
-				getFontSize = function() {
-					var size;
-					size = $this.css("font-size");
-					size = Number(size.substr(0, size.length - 2));
-					return size;
-				};
-				ppi = getPPI();
-				if (ppi > defaults.density) {
-					nFontSize = Math.round(ppi * getFontSize() / defaults.density) + "px";
+
+			/* devuelve los dpi de la pantalla del usuario */
+			getDPI = function() {
+				var DPI, element;
+				element = $("<div style='float: left; margin: 0; padding: 0; visibility: hidden; width: 1in;'></div>");
+				element.appendTo("body");
+				DPI = element.outerWidth();
+				element.remove();
+				return DPI;
+			};
+
+			/* devuelve el tamaño de fuente en pixeles */
+			getFontSize = function(element) {
+				var size;
+				size = element.css("font-size");
+				size = Number(size.substr(0, size.length - 2));
+				return size;
+			};
+
+			/* ajusta el tamaño de fuente */
+			adaptFontSize = function(element) {
+				var DPI, nFontSize;
+				DPI = getDPI();
+				if (DPI > defaults.density) {
+					nFontSize = Math.round(DPI * getFontSize(element) / defaults.density) + "px";
 				}
-				$this.css("font-size", nFontSize);
-				return true;
+				element.css("font-size", nFontSize);
+			};
+
+			/* ajusta el tamaño de las imágenes */
+			adaptImagesSize = function(element) {
+				var DPI, images;
+				DPI = getDPI();
+				if (DPI > defaults.density) {
+					images = $("img", element);
+					images.each(function() {
+						var $this, newHeight, newWidth;
+						$this = $(this);
+						newHeight = Math.round(DPI * $this.outerHeight() / defaults.density) + "px";
+						newWidth = Math.round(DPI * $this.outerWidth() / defaults.density) + "px";
+						return $this.css({
+							"height": newHeight,
+							"width": newWidth
+						});
+					});
+				}
+			};
+
+			/* aplica los cambios a los elementos seleccionados */
+			return this.each(function() {
+				var $this;
+				$this = $(this);
+				if (defaults.adaptFont) {
+					adaptFontSize($this);
+				}
+				if (defaults.adaptImages) {
+					adaptImagesSize($this);
+				}
 			});
 		};
 	})(jQuery);
